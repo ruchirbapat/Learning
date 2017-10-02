2b2f6318e80361782422bf5dc5d4853c760b6323
Ruchir Bapat <ruchir.bapat@gmail.com>
2017-10-03 01:47:50 +1100

    Removed some random code

--- Project/main.c
+++ Project/main.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
@@ -25,33 +25,4 @@ int main() {
 
 
 	return 0;
-}*/
-#include <stdio.h>
-
-void drawrect(int w, int h)
-{
-	int i, j;
-	for (i = 0; i < w; i++)
-		putchar('x');
-	putchar('\n');
-	for (i = 2; i < h; i++)
-	{
-		putchar('x');
-		for (j = 2; j < w; j++)
-			putchar(' ');
-		putchar('x');
-		putchar('\n');
-	}
-	for (i = 0; i < w; i++)
-		putchar('x');
-	putchar('\n');
-}
-
-int main()
-{
-	drawrect(10, 15);
-	drawrect(3, 5);
-	drawrect(50, 20);
-	drawrect(10, 10);
-	drawrect(2, 2);
 }
